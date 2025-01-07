c396c0715000aee30450f7df9895b5b617e8b5a5
samueldng <samuel-dng@outlook.com>
2025-01-07 13:45:28 -0300

    ajuste css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",  // Observando todos os arquivos na pasta app
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",  // Observando arquivos na pasta pages, se existir
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",  // Observando arquivos em components, se existir
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)",  // Usando variáveis CSS para o background
+        foreground: "var(--foreground)",  // Usando variáveis CSS para o foreground
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
