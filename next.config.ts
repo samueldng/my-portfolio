781c7530f3adc7fd2bd02b5272c0f832a8050548
samueldng <samuel-dng@outlook.com>
2025-01-07 00:29:36 -0300

    correcao de imagens e layout segunda tentativa

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/portfolio', // Definindo o caminho base do repositório
-  trailingSlash: true,    // Adiciona a barra no final da URL
+  output: 'export', // Exporta como uma aplicação estática
+  basePath: '/portfolio', // Caminho base para o repositório
+  assetPrefix: '/portfolio/', // Garante que os assets (imagens, fontes, etc) sejam carregados corretamente
+  trailingSlash: true, // Adiciona a barra no final da URL
 };
 
 export default nextConfig;
