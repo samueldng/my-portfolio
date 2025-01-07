4c48a61a81eadec3a590353f36050b4b63b227a7
Samuel Oliveira <63470561+samueldng@users.noreply.github.com>
2025-01-07 01:27:25 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "src/app/globals.css";
+import "/globals.css";
 
 // Configuração das fontes do Google
 const geistSans = Geist({
