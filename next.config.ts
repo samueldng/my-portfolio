2e8a19d58142ce58b0568c56c370f89a08ef0a58
samueldng <samuel-dng@outlook.com>
2025-01-07 00:19:46 -0300

    correcao de imagens e layout

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/portfolio', // Definindo o caminho base do repositório
+  trailingSlash: true,    // Adiciona a barra no final da URL
 };
 
 export default nextConfig;
